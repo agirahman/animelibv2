f260325134ee592b30d7945b5d16ac580374b5ec
agirahman <agi.rahman.s@gmail.com>
2026-02-14 17:11:34 +0700

    delete: scroll performance optimization

--- src/components/AnimeList/InfiniteScrollList.jsx
+++ src/components/AnimeList/InfiniteScrollList.jsx
@@ -59,7 +59,7 @@ const InfiniteScrollList = ({
 
     return (
         <>
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-8 content-visibility-auto">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-8">
                 {animeList.map((anime, index) => (
                     <AnimeCard
                         key={`${anime.id}-${index}`}
